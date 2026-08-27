273eb3ee884bc2e00aa2b6e8fd55466b24fc77ae
Sco314 <scottsandvik@gmail.com>
2026-08-26 20:27:05 -0500

    Load Overflow WHY engine

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -4,6 +4,7 @@ const CONTENT_FILES = [
   "src/content/capture-safety.js",
   "src/content/copy-polish.js",
   "src/content/selector-consistency.js",
+  "src/content/overflow-diagnostic.js",
   "src/content/picker.js"
 ];
 
